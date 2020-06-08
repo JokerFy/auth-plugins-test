061179960ec2b1108620bb6aa4fc35268437867d
Chris0621 <1301434395@qq.com>
2020-06-08 17:00:29 +0800

    Orm连接配置优化

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# auth-plugins-test
+# auth-plugins
+1.composer这个包之后，复制sql文件夹下的auth.sql到数据库运行，然后通过如下方法实例化Orm配置
+``
+$config = [
+    'type'     => 'mysql',
+    'hostname' => '127.0.0.1',
+    'username' => 'root',
+    'database' => 'mydb',
+    'password'  => '123456789',
+    'charset'  => 'utf8mb4',
+    'collation' => 'utf8mb4_general_ci',
+];
+\Finley\authPlugins\model\InitOrm::getInstance($config);
+``  
+2.单元测试文件写在tests文件下了，运行通过如 ../../vendor/bin/phpunit LoginTest.php
+来执行  
+3.模型使用了think-orm，可参考https://github.com/top-think/think-orm  
+4.验证器使用了think-validate，可参考https://github.com/top-think/think-validate  
+5.自己写了个异常处理，学习一下。  
+6.通过在控制器基类中require了common和database文件来初始化，要优化成参数传递的方式。  
+
--- index.php
+++ index.php
@@ -7,13 +7,21 @@
  */
 require_once './vendor/autoload.php';
 
-$capsule = new \Illuminate\Database\Capsule\Manager;
-// 创建链接
-$capsule->addConnection(require './conf/database.php');
-// 设置全局静态可访问DB
-$capsule->setAsGlobal();
-// 启动Eloquent （如果只使用查询构造器，这个可以注释）
-$capsule->bootEloquent();
-
-$controller = new \Finley\authPlugins\Controller\TestController();
-$controller->index();
+$config = [
+    'type'     => 'mysql',
+    'hostname' => '127.0.0.1',
+    'username' => 'root',
+    'database' => 'mydb',
+    'password'  => '123456789',
+    'charset'  => 'utf8mb4',
+    'collation' => 'utf8mb4_general_ci',
+];
+\Finley\authPlugins\model\InitOrm::getInstance($config);
+$controller = new \Finley\authPlugins\controller\Role();
+print_r($controller->getInfoById(1));
+//print_r($controller->insertRole(['role_name'=>"fangyi2",'remark'=>123456,'menu_id_list'=>[1,2,3]]));
+//print_r($controller->updateRole(['role_id'=>12,'role_name'=>"fangyi3",'remark'=>123456,'menu_id_list'=>[1,2,3]]));
+//print_r($controller->deleteRoleById(12));
+//print_r($controller->updateRoleById(['role_id'=>16,'role_name'=>"phpunit_test_update","remark"=>"987654321",'menu_id_list'=>"1,2,3"]));
+//print_r($controller->login(['username'=>'admin','password'=>'123123']));
+//print_r($controller->updateUser(['user_id'=>7,'username'=>"fangyi2",'password'=>123456,'role_id_list'=>[3]]));
